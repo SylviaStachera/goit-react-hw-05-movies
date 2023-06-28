ca647d67d2fe7c08167dc496c3890876cf8ae632
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-06-28 17:22:09 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,43 +1,44 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useLocation } from "react-router-dom";
-import { getTrending } from "../../components/services/api";
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import { getTrending } from '../../components/services/api';
 
-import css from "./Home.module.css";
+import css from './Home.module.css';
 
 const Home = () => {
-	const [trendingMovies, setTrendingMovies] = useState([]);
-	const [isLoading, setIsLoading] = useState(false);
-	const location = useLocation();
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const location = useLocation();
 
-	useEffect(() => {
-		setIsLoading(true);
+  useEffect(() => {
+    setIsLoading(true);
 
-		getTrending()
-			.then((data) => {
-				setTrendingMovies(data.results);
-				setIsLoading(false);
-			})
-			.catch((error) => console.log(error));
-	}, []);
+    getTrending()
+      .then(data => {
+        setTrendingMovies(data.results);
+        setIsLoading(false);
+      })
+      .catch(error => console.log(error));
+  }, []);
 
-	return (
-		<div className={css["list-container"]}>
-			<h1>Trending today</h1>
-			<ul>
-				{trendingMovies.map((movie) => {
-					return (
-						<li key={movie.id}>
-							<Link to={`/movies/${movie.id}`} state={{ from: location }}>
-								{" "}
-								{movie.title}
-							</Link>
-						</li>
-					);
-				})}
-			</ul>
-		</div>
-	);
+  return (
+    <div className={css['list-container']}>
+      <h1>Trending today</h1>
+      <ul>
+        {isLoading && <p>Lodaing...</p>}
+        {trendingMovies.map(movie => {
+          return (
+            <li key={movie.id}>
+              <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+                {' '}
+                {movie.title}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
