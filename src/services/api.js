05e02380bbaffb14cb9c63d503f07766851a638e
Sylvia Stachera <113374423+SylviaStachera@users.noreply.github.com>
2023-06-30 18:48:28 +0200

    Add axios.defaults.params

--- src/services/api.js
+++ src/services/api.js
@@ -1,37 +1,36 @@
 import axios from 'axios';
 
-const apiKey = '5b510caffd74cc663774c058405dd762';
 axios.defaults.baseURL = 'https://api.themoviedb.org/3/';
 
+axios.defaults.params = {
+  api_key: '5b510caffd74cc663774c058405dd762',
+};
+
 export const getTrending = async () => {
-  const response = await axios.get(`trending/movie/day?api_key=${apiKey}`);
+  const response = await axios.get(`trending/movie/day?`);
   return response.data;
 };
 
 export const searchMovies = async query => {
   const response = await axios.get(
-    `search/movie?api_key=${apiKey}&query=${query}&include_adult=false&language=en-US&page=1`
+    `search/movie?&query=${query}&include_adult=false&language=en-US&page=1`
   );
   return response.data;
 };
 
 export const getMovieDetails = async movieId => {
-  const response = await axios.get(
-    `movie/${movieId}?api_key=${apiKey}&language=en-US`
-  );
+  const response = await axios.get(`movie/${movieId}?language=en-US`);
   return response.data;
 };
 
 export const getMovieCredits = async movieId => {
-  const response = await axios.get(
-    `movie/${movieId}/credits?api_key=${apiKey}&language=en-US`
-  );
+  const response = await axios.get(`movie/${movieId}/credits?language=en-US`);
   return response.data;
 };
 
 export const getMovieReviews = async movieId => {
   const response = await axios.get(
-    `movie/${movieId}/reviews?api_key=${apiKey}&language=en-US&page=1`
+    `movie/${movieId}/reviews?language=en-US&page=1`
   );
   return response.data;
 };
